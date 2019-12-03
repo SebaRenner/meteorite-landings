9c1f2a2932acb5e6fef268b2ace08a9771d55345
Sebastian Renner <sebastian.renner@students.fhnw.ch>
2019-12-03 12:01:53 +0100

    Added API Key

--- meteorite/src/App.js
+++ meteorite/src/App.js
@@ -4,41 +4,31 @@ import './App.css';
 import Chart from 'react-google-charts';
 
 function App() {
-
-    var googleApiKey = "AIzaSyDEY6hLnwxQ61vIcIEi5-PLDla86XMp2dU";
-
-  return (
-
+    // var googleApiKey = "AIzaSyDEY6hLnwxQ61vIcIEi5-PLDla86XMp2dU";
+    return (
       <Chart
           width={'1000px'}
           height={'800px'}
           chartType="GeoChart"
           data={[
-              ['City',   'Population', 'Area'],
-              ['Rome',      2761477,    1285.31],
-              ['Milan',     1324110,    181.76],
-              ['Naples',    959574,     117.27],
-              ['Turin',     907563,     130.17],
-              ['Palermo',   655875,     158.9],
-              ['Genoa',     607906,     243.60],
-              ['Bologna',   380181,     140.7],
-              ['Florence',  371282,     102.41],
-              ['Fiumicino', 67370,      213.44],
-              ['Anzio',     52192,      43.43],
-              ['Ciampino',  38262,      11]
+              ['Country', 'Popularity'],
+              ['Germany', 200],
+              ['United States', 300],
+              ['Brazil', 400],
+              ['Canada', 500],
+              ['France', 600],
+              ['Russia', 700]
           ]}
           options = {{
-          region: 'IT',
           displayMode: 'markers',
           colorAxis: {colors: ['yellow', 'blue']}
           }}
           // Note: you will need to get a mapsApiKey for your project.
           // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-          mapsApiKey = "AIzaSyDEY6hLnwxQ61vIcIEi5-PLDla86XMp2dU"
+          mapsApiKey = "AIzaSyBCtntq40U0DMUdry8-Nr4fLy8WbfOb5GM"
           // no idea what this does
           rootProps={{ 'data-testid': '1' }}
       />
-
   );
 }
 
