7773450e858ade79949b9eb8eea191feb6665158
Alain <alain.icu@gmail.com>
2019-12-23 17:07:40 +0100

    changed programm settings to be n.js friendly

--- meteorite/src/App.js
+++ meteorite/src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import './App.css';
 
 /*
@@ -13,8 +14,26 @@ data[0].year;
  */
 
 class App extends Component {
+  state = {
+    lat: 51.505,
+    lng: -0.09,
+    zoom: 13,
+  }
   render() {
-    return <p></p>
+    const position = [51.505,-0.09]
+    return (
+        <Map className={"Map"} center={position} zoom= {13} >
+          <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </Map>
+    )
   }
 }
 
